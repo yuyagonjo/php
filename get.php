75710e74d4ad490fe0d72f2dbee0cb9eb1ff6b32
yuyagonjo <gon0616yuya28@gmail.com>
2016-01-31 22:15:36 +0900

    rectuer96

---
+++ get.php
@@ -0,0 +1,8 @@
+<?php 
+
+$first_name = $_REQUEST['fname'];
+$last_name = $_REQUEST['lname'];
+$secret = $_REQUEST['secret1'];
+echo "the hidden info is " .$secret;
+
+?>
---
+++ grab.php
@@ -0,0 +1,8 @@
+<?php 
+
+$first_name = $_POST['fname'];
+$last_name = $_POST['lname'];
+
+echo "the first name is".$first_name.", last name is".$last_name;
+
+?>
