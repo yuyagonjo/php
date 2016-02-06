2b1ef920df2f978749e934920d5ea0e4f98985ec
yuyagonjo <gon0616yuya28@gmail.com>
2016-02-06 16:33:18 +0900

    rectuer100

---
+++ section1.php
@@ -0,0 +1,29 @@
+
+<?php include('header.php') ?>
+
+<body>
+	<div id="header">
+		<h1>My Website</h1>
+	</div>
+	<div id="navigation">
+		<ul>
+			<li><a href="section1.php">Section 1</a></li>
+			<li><a href="section2.php">Section 2</a></li>
+			<li><a href="section3.php">Section 3</a></li>
+			<li><a href="section4.php">Section 4</a></li>
+			<li><a href="#">Coming Soon</a></li>
+		</ul>
+	</div>
+	
+	<div id="content">
+<!-- this is section1 -->
+
+		<h1>Section 1</h1>
+			
+			<p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
+			
+	</div>
+	<?php include('footer.php'); ?>	
+</body>
+
+</html>
